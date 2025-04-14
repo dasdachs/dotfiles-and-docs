536b1bb605fd11eb32b1917a78295e7ac1dc38ff
dasdachs <jani.sumak@gmail.com>
2025-04-14 16:44:25 +0200

    Updated codecompanion base models

--- .config/nvim/after/plugin/completion.rc.lua
+++ .config/nvim/after/plugin/completion.rc.lua
@@ -16,13 +16,45 @@ require("codecompanion").setup({
         },
       })
     end,
+    deepcoder1_5 = function()
+      return require("codecompanion.adapters").extend("ollama", {
+        name = "deepcoder1.5",
+        schema = {
+          model = {
+            default = "deepcoder:1.5b",
+          },
+          num_ctx = {
+            default = 16384,
+          },
+          num_predict = {
+            default = -1,
+          },
+        },
+      })
+    end,
+    deepcoder = function()
+      return require("codecompanion.adapters").extend("ollama", {
+        name = "deepcoder",
+        schema = {
+          model = {
+            default = "deepcoder:14b-preview-q4_K_M",
+          },
+          num_ctx = {
+            default = 16384,
+          },
+          num_predict = {
+            default = -1,
+          },
+        },
+      })
+    end,
   },
   strategies = {
     chat = {
-      adapter = "qwen14b",
+      adapter = "deepcoder",
     },
     inline = {
-      adapter = "qwen14b",
+      adapter = "deepcoder1_5",
     },
     opts = {
       -- Set debug logging
